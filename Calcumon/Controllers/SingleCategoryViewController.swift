925e71684c1067b2e2ca019acf9c82b7082e9d1b
SarinSwift <sarinyaswift@gmail.com>
2019-02-15 18:12:21 -0800

    added views with a textlabel

--- Calcumon/Controllers/SingleCategoryViewController.swift
+++ Calcumon/Controllers/SingleCategoryViewController.swift
@@ -12,17 +12,15 @@ class SingleCategoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-        
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         setupNavBar()
+        
     }
     
     func setupNavBar() {
         self.title = "Math category"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backToMainPage))
     }
-    
     @objc func backToMainPage() {
         self.navigationController?.dismiss(animated: true, completion: nil)
     }
