85a5605fddd2040b03e254103322b9f9b6bb16d1
SarinSwift <sarinyaswift@gmail.com>
2019-02-23 10:53:47 -0800

    took out mock data and replaced with custom math problems

--- Calcumon/Controllers/SingleCategoryViewController.swift
+++ Calcumon/Controllers/SingleCategoryViewController.swift
@@ -12,16 +12,16 @@ class SingleCategoryViewController: UIViewController {
     
     // Data that is similar to what we get from the API
     // TODO: When calling the API, we need to convert from mathML markup to the actual equation in text form
-    var mockData: Equation = Equation(id: "pccnpg",
-                                      question: "<mn>2<\\/mn><mi>x<\\/mi><mo> - <\\/mo><mn>1<\\/mn><mo> = <\\/mo><mn>5<\\/mn>",
-                                      choice: ["<math><mo>-<\\/mo><mn>4<\\/mn><\\/math>",
-                                                "<math><mn>4<\\/mn><\\/math>",
-                                                "<math><mo>-<\\/mo><mn>10<\\/mn><\\/math>",
-                                                "<math><mn>3<\\/mn><\\/math>",
-                                                "<math><mn>1<\\/mn><\\/math>"
-                                                ],
-                                      correctChoice: 3,
-                                      instruction: "Solve for x")
+//    var mockData: Equation = Equation(id: "pccnpg",
+//                                      question: "<mn>2<\\/mn><mi>x<\\/mi><mo> - <\\/mo><mn>1<\\/mn><mo> = <\\/mo><mn>5<\\/mn>",
+//                                      choice: ["<math><mo>-<\\/mo><mn>4<\\/mn><\\/math>",
+//                                                "<math><mn>4<\\/mn><\\/math>",
+//                                                "<math><mo>-<\\/mo><mn>10<\\/mn><\\/math>",
+//                                                "<math><mn>3<\\/mn><\\/math>",
+//                                                "<math><mn>1<\\/mn><\\/math>"
+//                                                ],
+//                                      correctChoice: 3,
+//                                      instruction: "Solve for x")
 
     @IBOutlet weak var topContainerView: UIView!
     @IBOutlet weak var solveView: UIView!
@@ -52,12 +52,13 @@ class SingleCategoryViewController: UIViewController {
     
     // currently using mockData
     func getRandomQuestion() {
-        questionLabel.text = mockData.question
-        choice1.setTitle(mockData.choice[0], for: .normal)
-        choice2.setTitle(mockData.choice[1], for: .normal)
-        choice3.setTitle(mockData.choice[2], for: .normal)
-        choice4.setTitle(mockData.choice[3], for: .normal)
-        choice5.setTitle(mockData.choice[4], for: .normal)
+        questionLabel.text = MathExpression(lhs: .Integer(value: 10), rhs: .Percentage(value: 20), operator: .minus).description
+        
+        choice1.setTitle("blah", for: .normal)
+        choice2.setTitle("blah", for: .normal)
+        choice3.setTitle("blah", for: .normal)
+        choice4.setTitle("blah", for: .normal)
+        choice5.setTitle("blah", for: .normal)
     }
 
     @IBAction func continueButtonPressed(_ sender: UIButton) {
