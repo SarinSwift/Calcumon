74b1ea8ead165ab28e025b822de79d71eac2aca0
SarinSwift <sarinyaswift@gmail.com>
2019-02-22 23:01:15 -0800

    working on randomly generated math problems

--- Calcumon/AppDelegate.swift
+++ Calcumon/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Thread.sleep(forTimeInterval: 0.03)
         return true
     }
 
---
+++ Calcumon/NetworkManager.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkManager.swift
+//  Calcumon
+//
+//  Created by Sarin Swift on 2/19/19.
+//  Copyright © 2019 sarinswift. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    let urlSession = URLSession.shared
+    var baseUrl = "http://api.wolframalpha.com/v1/result"
+    var appId = "K885QJ-6U8J5Y27AG"
+    
+    func getPost() {
+        
+    }
+}
