f6dd6a0ca97a6b3759008930c34e1c79f6bce30f
SarinSwift <sarinyaswift@gmail.com>
2019-03-02 15:16:47 -0800

    added gradient for results UIViews background

--- Calcumon/Controllers/ProfileLogViewController.swift
+++ Calcumon/Controllers/ProfileLogViewController.swift
@@ -16,7 +16,16 @@ class ProfileLogViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = #colorLiteral(red: 0.8699956536, green: 0.7443540692, blue: 0.8824878335, alpha: 1)
+        setupBgGradient()
+    }
+    
+    func setupBgGradient() {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.startPoint = CGPoint(x: 0.5, y: 1)
+        gradient.endPoint = CGPoint(x: 1, y: 0)
+        gradient.colors = [#colorLiteral(red: 0.7921568627, green: 0.5176470588, blue: 0.8862745098, alpha: 1).cgColor, #colorLiteral(red: 0.4, green: 0.2588235294, blue: 0.7529411765, alpha: 1).cgColor]
+        view.layer.insertSublayer(gradient, at: 0)
     }
     
     @IBAction func downBtnTapped(_ sender: UIButton) {
