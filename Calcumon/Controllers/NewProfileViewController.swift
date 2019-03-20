6c07d7faef158583d304c572719b63081d2541a2
SarinSwift <sarinyaswift@gmail.com>
2019-03-20 12:23:56 -0700

    added new viewController for signing up as a new user

---
+++ Calcumon/Controllers/NewProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewProfileViewController.swift
+//  Calcumon
+//
+//  Created by Sarin Swift on 3/20/19.
+//  Copyright © 2019 sarinswift. All rights reserved.
+//
+
+import UIKit
+
+class NewProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func CancelBtnTapped(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+}
