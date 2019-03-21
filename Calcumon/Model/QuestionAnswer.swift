1a1b81619ad1ba491a04ad1d38d1f2570219d7a6
SarinSwift <sarinyaswift@gmail.com>
2019-03-21 16:59:37 -0700

    added question/answer model to keep track append in array as users complete questions. So we can populate the tableview

---
+++ Calcumon/Model/QuestionAnswer.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionAnswer.swift
+//  Calcumon
+//
+//  Created by Sarin Swift on 3/21/19.
+//  Copyright © 2019 sarinswift. All rights reserved.
+//
+
+import Foundation
+
+class QuestionAnswer {
+    let question: String
+    let answer: String
+    
+    init(question: String, answer: String) {
+        self.question = question
+        self.answer = answer
+    }
+}
---
+++ Calcumon/Views/QuestionLogCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionLogCell.swift
+//  Calcumon
+//
+//  Created by Sarin Swift on 3/21/19.
+//  Copyright © 2019 sarinswift. All rights reserved.
+//
+
+import UIKit
+
+class QuestionLogCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
